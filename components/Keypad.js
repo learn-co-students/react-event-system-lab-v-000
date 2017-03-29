7e7c5070de5bfe4ea7a2f21daa8991f55727c255
Ann Lee <aplee29@users.noreply.github.com>
2017-03-28 21:01:07 -0400

    Done.

---
+++ components/Keypad.js
@@ -0,0 +1,23 @@
+const React = require('react');
+
+class Keypad extends React.Component {
+  constructor() {
+    super();
+
+    this.handlePassword = this.handlePassword.bind(this);
+  }
+
+  handlePassword() {
+    console.log('Entering password...');
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="password" onKeyUp={this.handlePassword} />
+      </div>
+    );
+  }
+}
+
+module.exports = Keypad;
