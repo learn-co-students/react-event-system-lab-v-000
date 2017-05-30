d319d535ec3341f11899b01ede22444e53332995
github username <krishel.lasam@gmail.com>
2017-05-29 20:51:24 -0400

    Done.

---
+++ components/EyesOnMe.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default class EyesOnMe extends React.Component {
+  constructor() {
+    super()
+    this.focus = this.focus.bind(this)
+    this.blur = this.blur.bind(this)
+  }
+
+  focus() {
+    console.log('Good!')
+  }
+
+  blur() {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+    return (
+      <button onFocus={this.focus} onBlur={this.blur}>Here</button>
+    )
+  }
+}
