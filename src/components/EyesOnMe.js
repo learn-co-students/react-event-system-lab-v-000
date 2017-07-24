30e4f3626041f95ee1e153522d6cf03a2b4994b5
Trai Compton <tlcompton2546@outlook.com>
2017-07-24 22:06:19 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+    constructor(){
+        super();
+        
+        this.eyeContact = this.eyeContact.bind(this);
+        this.driftOff = this.driftOff.bind(this);
+    }
+        
+    eyeContact(){
+        console.log('Good!');
+    }
+        
+    driftOff(){
+        console.log('Hey! Eyes on me!');
+    }
+    
+    render(){
+        return(
+            <button onFocus={ this.eyeContact } onBlur={ this.driftOff }></button>
+        );
+    }
+}
+
+export default EyesOnMe;
