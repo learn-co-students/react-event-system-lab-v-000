a3a816150778623a299dbb5d41e312984eed487e
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-08-08 14:30:15 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+// Code EyesOnMe Component
+import React from 'react'
+
+export default class EyesOnMe extends React.Component{
+  render(){
+    return (<button onFocus={this.focusGained} onBlur={this.focusLost}></button>)
+  }
+
+  focusGained(){
+    console.log('Good!');
+  }
+
+  focusLost(){
+    console.log('Hey! Eyes on me!');
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from 'react'
+
+export default class Keypad extends React.Component{
+  render(){
+    return (<input type="password" onKeyUp={this.keyPressed} />)
+  }
+
+  keyPressed(){
+    console.log('Entering password...');
+  }
+}
