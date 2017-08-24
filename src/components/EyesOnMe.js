b65f49a7725c70f09356174d622949bdb33ccdc8
lucketta <lucketta@umich.edu>
2017-08-23 23:21:18 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+export default class EyesonMe extends React.Component {
+  onFocus = () => {
+   console.log('Good!');
+ }
+ onBlur = () => {
+  console.log('Hey! Eyes on me!');
+}
+
+ render() {
+   return (
+     <button onBlur={this.onBlur} onFocus={this.onFocus} />
+   );
+ }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
-// Code Keypad Component Here
+import React from 'react'
+
+export default class Keypad extends React.Component {
+  inputPassword = () => {
+   console.log('Entering password...');
+ }
+
+ render() {
+   return (
+     <input type="password" onKeyUp={this.inputPassword} />
+   );
+ }
+}
