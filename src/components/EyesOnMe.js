5956ee4f138f7664a05cb03832e518a8df816bd1
Bob Habig <rfhab2@gmail.com>
2017-08-28 00:56:41 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,32 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component {
+  // constructor() {
+  //   super();
+  //
+  //   this.onBlur = this.onBlur.bind(this);
+  //   this.onFocus = this.onFocus.bind(this);
+  // }
+  //
+  // onBlur() {
+  //   console.log('Hey! Eyes on me!')
+  // };
+  //
+  // onFocus() {
+  //   console.log('Good!')
+  // };
+
+  onFocus = () => {
+    console.log('Good!');
+  }
+
+  onBlur = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+  render() {
+    return (
+      <button onBlur={this.onBlur} onFocus={this.onFocus}>Eyes on me, please!</button>
+    );
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
-// Code Keypad Component Here
+import React from 'react';
+
+export default class Keypad extends React.Component {
+  // constructor() {
+  //   super();
+  //
+  //   this.onTyping = this.onTyping.bind(this);
+  // }
+  // onTyping() {
+  //   console.log('Entering password...')
+  // };
+
+  onTyping = () => {
+    console.log('Entering password...');
+  }
+
+  render() {
+    return (
+      <input type="password" onKeyUp={this.onTyping} />
+    );
+  }
+}
