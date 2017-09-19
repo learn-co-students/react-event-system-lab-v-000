1665545678524ee4763ee543a865f07131bf8432
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-19 15:48:14 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+  blurring = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+  focusing = () => {
+    console.log('Good!')
+  }
+
+  render(){
+    return (
+      <button onBlur={this.blurring} onFocus={this.focusing} ></button>
+    )
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component{
+
+  typing = () => {
+    console.log("Entering password...")
+  }
+
+  render(){
+    return (
+      <div>
+        <input onKeyUp={this.typing} type="password" />
+      </div>
+    )
+  }
+}
+
+export default Keypad
