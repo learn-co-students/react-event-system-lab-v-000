c6821d918d3a268b5bbb5a1431fe6855134ac367
Bruce Molina <bruce.molina.81@gmail.com>
2017-10-24 01:22:25 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+
+export default class EyesOnMe extends React.Component{
+
+
+  focused = ()=>{console.log("Good!")}
+  blurred = ()=>{console.log("Hey! Eyes on me!")}
+  pasted = ()=>{console.log("you pasted something!")}
+
+  render(){
+    return(
+      <button onFocus={this.focused} onPaste={this.pasted} onBlur={this.blurred}type="button">Click Me!</button>
+    )
+  }
+}
