e1813f5124951f1ffc63763624ed9a5340fb6da0
ndalcin <dalcin.nicole@gmail.com>
2018-01-24 10:08:32 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component{
+
+  focus = () => {
+    console.log("Good!")
+  }
+  blur = () => {
+    console.log("Hey! Eyes on me!")
+  }
+  render() {
+    return (
+      <button onFocus={this.focus} onBlur={this.blur}>Eyes on Me!</button>
+    );
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+  password = () => {
+    console.log('Entering password...')
+  };
+
+  render(){
+    return (
+      <input type='password' onKeyUp={this.password}></input>
+    )
+  }
+}
+
+
+
+export default Keypad;
