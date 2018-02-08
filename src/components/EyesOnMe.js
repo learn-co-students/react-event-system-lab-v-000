282301f376bb5cf7943f3e8cb6503676f65e5592
Bob Habig <rfhab2@gmail.com>
2018-02-08 10:20:49 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,32 +1,19 @@
 import React from 'react';
 
 export default class EyesOnMe extends React.Component {
-  // constructor() {
-  //   super();
-  //
-  //   this.onBlur = this.onBlur.bind(this);
-  //   this.onFocus = this.onFocus.bind(this);
-  // }
-  //
-  // onBlur() {
-  //   console.log('Hey! Eyes on me!')
-  // };
-  //
-  // onFocus() {
-  //   console.log('Good!')
-  // };
-
-  onFocus = () => {
-    console.log('Good!');
+  isFocusing = (e) => {
+    console.log('Good!')
   }
 
-  onBlur = () => {
-    console.log('Hey! Eyes on me!');
+  isNotFocusing = (e) => {
+    console.log('Hey! Eyes on me!')
   }
 
   render() {
-    return (
-      <button onBlur={this.onBlur} onFocus={this.onFocus}>Eyes on me, please!</button>
-    );
+    return(
+      <div>
+        <button onFocus={this.isFocusing} onBlur={this.isNotFocusing} >Attention!!</button>
+      </div>
+    )
   }
 }
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,22 +1,16 @@
 import React from 'react';
 
 export default class Keypad extends React.Component {
-  // constructor() {
-  //   super();
-  //
-  //   this.onTyping = this.onTyping.bind(this);
-  // }
-  // onTyping() {
-  //   console.log('Entering password...')
-  // };
 
-  onTyping = () => {
-    console.log('Entering password...');
+  pwdFn = (e) => {
+    console.log('Entering password...')
   }
 
   render() {
-    return (
-      <input type="password" onKeyUp={this.onTyping} />
-    );
+    return(
+      <div>
+        <input type="password" onKeyUp={this.pwdFn} />
+      </div>
+    )
   }
 }
