69d2358f66777450178ec2c888ab847a8a208d1e
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2018-02-21 18:37:29 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+    a = () => {
+        console.log('Good!');
+    }
+    
+    b = () => {
+        console.log('Hey! Eyes on me!');
+    }
+    
+    render() {
+        return ( <button onFocus={this.a} onBlur={this.b}>Button</button> );
+    }
+}
+
+export default EyesOnMe
