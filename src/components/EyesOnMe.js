f9b5c440286befb868c95492acd88b05a5663abf
jss530 <jsifre01@gmail.com>
2018-03-16 21:35:40 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,8 +1,3 @@
-// In the components/EyesOnMe.js file, create a EyesOnMe React component.
-// In that component, render a button.
-// On that button, add event handlers that listens for the focus and blur events.
-// When the focus event fires, use console.log to print out the text 'Good!'.
-// When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
 
 import React from 'react';
 
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,7 +1,3 @@
-// In the components/Keypad.js file, create a Keypad React component.
-// In that component, render an input with the right type.
-// On that input, add an event handler that listens for the keyUp event.
-// When that event fires, use console.log to print out the text 'Entering password...'.
 
 import React from 'react';
 
