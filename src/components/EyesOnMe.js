e45150fd41e556007d874804d0095510263c8c07
Alexander Urizar <urizaraj@gmail.com>
2018-04-13 14:12:24 -0400

    Completing the lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+  good = () => console.log('Good!')
+  eyes = () => console.log('Hey! Eyes on me!')
+
+  render() {
+    return (
+      <button onFocus={this.good} onBlur={this.eyes} />
+    );
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
-// Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component {
+  message = () => console.log('Entering password...')
+
+  render() {
+    return (
+      <input type="password" onKeyUp={this.message} />
+    )
+  }
+}
+
+export default Keypad
