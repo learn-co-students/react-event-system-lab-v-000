208c6b054c428acd867b6a74042635bc8b51e404
meq <fewell@gmail.com>
2018-05-15 16:07:53 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+export default class EyesOnMe extends React.Component {
+  tickle = () => {
+     console.log('Good!');
+   }
+
+   fickle = () => {
+      console.log('Hey! Eyes on me!');
+    }
+
+   render() {
+     return (
+       <button onFocus={this.tickle} onBlur={this.fickle}>Tickle me!</button>
+     );
+   }
+ }
