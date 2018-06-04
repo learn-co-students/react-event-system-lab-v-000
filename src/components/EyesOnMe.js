a8f108b78d2e419e326f5a3fca9112ca6594f086
shravnk <brendan.ode4@gmail.com>
2018-06-04 03:02:51 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
+// src/components/ToggleButton.js
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  logFocus = () => {
+    console.log('Good!');
+  }
+
+  logBlur = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+    return (
+      <button onFocus={this.logFocus} onBlur={this.logBlur}/>
+    )
+  }
+}
+
+export default EyesOnMe;// Bomb Component Code Goes Here
+// Code Keypad Component Here
 // Code EyesOnMe Component Here
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
+// src/components/ToggleButton.js
+import React from 'react';
+
+class Keypad extends React.Component {
+  logPassword = () => {
+    console.log('Entering password...');
+  }
+
+  render() {
+    return (
+      <input onKeyUp={this.logPassword} type="password"></input>
+    )
+  }
+}
+
+export default Keypad;// Bomb Component Code Goes Here
 // Code Keypad Component Here
