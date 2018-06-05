7bb72fc23091b78a9eb7be7d65ef8a8c21d5e77c
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-05 14:48:47 -0400

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
-// Code Keypad Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Keypad extends React.Component {
+
+  //how do i know where to put my variables and methods?
+  //before render, after render before return?
+
+  output = () => {
+    console.log('Entering password...')
+  }
+
+  render() {
+    return (
+      <div className="keypad">
+        <input type="password" onKeyUp={this.output}></input>
+      </div>
+    )
+  }
+}
+
+export default Keypad;
