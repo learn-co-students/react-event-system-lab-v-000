8bf166372b0db7de890fb5efb60fd8503f89683a
Chaim Reisman <chaim@Chaims-MacBook-Pro.local>
2018-06-24 14:18:06 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+export default class EyesOnMe extends React.Component {
+  onFocus = () => {
+    console.log('Good!')
+  }
+  onBlur = () => {
+    console.log('Hey! Eyes on me!')
+  }
+  render () {
+    return (
+      <div>
+        <button onFocus={ this.onFocus } onBlur={ this.onBlur } >Focus </button>
+      </div>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+export default class Keypad extends React.Component {
+  typing = () => {
+    console.log('Entering password...');
+  }
+  render () {
+    return (
+      <div>
+        <input type="password" onKeyUp={this.typing}></input>
+      </div>
+    )
+  }
+}
