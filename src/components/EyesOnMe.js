171335ef79f7a32fc0db75a692db29c32f6adcab
Kelsey Fecho <kfecho30@gmail.com>
2018-07-24 13:32:49 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component{
+
+  good = () => {
+    console.log('Good!');
+  }
+
+  attn = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+  render() {
+    return(
+      <button onFocus={this.good} onBlur={this.attn}></button>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import React from 'react';
+
+export default class Keypad extends React.Component{
+
+  entering = () => {
+    console.log('Entering password...')
+  }
+
+  render() {
+    return(
+      <form>
+        <input type='password' onKeyUp={this.entering}></input>
+      </form>
+    )
+  }
+}
