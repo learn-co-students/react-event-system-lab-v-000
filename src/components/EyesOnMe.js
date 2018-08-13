8b0eec15170046cc339ef37470b1fa1a222f155f
AspenJames <aspenjames@tqca.org>
2018-08-13 15:01:39 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+  
+  render(){
+    return <button onFocus={this.focusHandler} onBlur={this.blurHandler}>Eyes On Me</button>
+  }
+
+  focusHandler = () => {
+    console.log('Good!')
+  }
+
+  blurHandler = () => {
+    console.log('Hey! Eyes on me!')
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+
+  render(){
+    return <input id="password" type="password" onKeyUp={this.keyUpHandler} />
+  }
+
+  keyUpHandler = () => {
+    console.log('Entering password...')
+  }
+}
+
+
+export default Keypad
