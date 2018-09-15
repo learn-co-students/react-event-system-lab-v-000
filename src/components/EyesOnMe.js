9fe86a759e0e65b0af7599bedb9a1557ba59b129
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-09-14 18:01:16 -0700

    init commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+  render() {
+    return (
+      <div>
+        <button
+          onFocus={() => console.log('Good!')}
+          onBlur={() => console.log('Hey! Eyes on me!')}
+        />
+      </div>
+    );
+  }
+}
