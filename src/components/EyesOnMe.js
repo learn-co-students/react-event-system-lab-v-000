2994443689dd72f892fc525793a7f939fbb41570
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-21 10:23:48 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -5,9 +5,11 @@ export default class EyesOnMe extends Component {
   focusEvent = () =>{
     return console.log("Good!")
   }
+
   blurEvent = () =>{
     return console.log("Hey! Eyes on me!")
   }
+
   render(){
     return (
       <button onFocus = {this.focusEvent} onBlur = {this.blurEvent}></button>
