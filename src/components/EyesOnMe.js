30a2dadefd408ad1b359301e2e703ef84da58d84
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-10-06 16:43:35 -0400

    Implement Keypad to spec

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,8 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EeysOnMe extends Component {
+    render() {
+        return <button>In The Club</button>
+    }
+}
