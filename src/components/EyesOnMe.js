70134dfb30629b442f3e910be763ed0a6f101633
VictoriaMeng <1ststarofthenight@gmail.com>
2018-10-07 22:05:07 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class EyesOnMe extends React.Component {
+  printGood = (event) => {
+    console.log("Good!")
+  }
+
+  printEyes = (event) => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+    return (
+      <button onFocus={this.printGood} onBlur={this.printEyes} ></button>
+    )
+  }
+};
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Keypad extends React.Component {
+  printPWMessage = (event) => {
+    console.log('Entering password...')
+  }
+
+  render() {
+    return (
+      <input type="password" onKeyUp={this.printPWMessage} />
+    )
+  }
+};
