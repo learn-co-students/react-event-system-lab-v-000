df91fc10c66840e07093d1e44d53c48abdf77e9f
github username <github email address>
2018-11-22 13:10:06 -0800

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
-// Code EyesOnMe Component Here
+
+import React, { Component } from 'react';
+
+
+export default class EyesOnMe extends Component {
+    constructor(){
+        super()
+    }
+
+    handleOnFocus = (event) => {
+        console.log("Good!");
+    }
+
+    handleOnBlur = (event) => {
+        console.log('Hey! Eyes on me!');
+    }
+
+    render(){
+        return(
+            <div className='EyesOnMe'>
+                <button onBlur={ this.handleOnBlur } onFocus={ this.handleOnFocus }>submit</button>
+            </div>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,26 @@
-// Code Keypad Component Here
+
+import React, { Component } from 'react';
+
+
+export default class Keypad extends Component {
+    constructor(){
+        super()
+    }
+
+
+    handleOnKeyUp = (event) => {
+
+        console.log("Entering password...");
+    }
+
+    render(){
+        return (
+            <div className='keypad'>
+                <form>
+                    Password: <input type='password' onKeyUp={ this.handleOnKeyUp }/>
+
+                </form>
+            </div>
+        )
+    }
+}
