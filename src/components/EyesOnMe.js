908b1b3d6003f2dcbe60f2805d7d32274f631acb
Sara Stanton <sara.mpa13@gmail.com>
2019-01-04 02:03:42 -0600

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+  render() {
+    return (
+      <button onFocus={this.handleFocus} onBlur={this.handleBlur}> </button>
+    )
+  }
+
+  handleFocus = () => {
+    console.log("Good!")
+  }
+
+  handleBlur = () => {
+    console.log("Hey! Eyes on me!")
+  }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+  render() {
+    return (
+      <input type="password" onKeyUp={this.handleKey}/>
+    )
+  }
+
+  handleKey = () => {
+    console.log("Entering password...")
+  }
+
+
+}
