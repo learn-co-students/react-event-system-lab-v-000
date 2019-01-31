993577014992f3e6563cc906d9c6fbb2d1c92cf7
Micah Shute <micah.shute@gmail.com>
2019-01-31 01:59:01 -0600

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+
+    constructor(props){
+        super(props)
+    }
+
+    handleFocus = () => {
+        console.log("Good!")
+    }
+
+    handleBlur = () => {
+        console.log("Hey! Eyes on me!")
+    }
+
+    render(){
+        return (
+            <button onBlur={this.handleBlur} onFocus={this.handleFocus}></button>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
-// Code Keypad Component Here
+import React, { Component } from 'react'
+
+export default class Keypad extends Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    handlePasswordEntry = () => {
+        console.log("Entering password...")
+    }
+
+    render(){
+        return (
+            <input type="password" onKeyUp={this.handlePasswordEntry}></input>
+        )
+    }
+}
