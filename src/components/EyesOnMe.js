f39f7df3749556a115495d87af72624df0642e68
Vibraniumforge <gregm9999@gmail.com>
2019-02-03 12:50:14 -0800

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
-// Code EyesOnMe Component Here
+import React, {Component} from 'react'
+
+
+class EyesOnMe extends Component {
+
+    focusHandler() {
+        console.log("Good!");
+    }
+
+    blurHandler() {
+        console.log("Hey! Eyes on me!")
+    }
+
+    render() {
+        return (
+            <div>
+                <button type="button" onFocus={this.focusHandler} onBlur={this.blurHandler}>Button</button>
+            </div>
+        )
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import React, {Component } from 'react'
+
+class Keypad extends Component {
+
+    passwordHandler() {
+        console.log("Entering password...");
+    }
+    render() {
+        return (
+            <div><input type= "password" id="password" placeholder="Enter password" onKeyUp={this.passwordHandler}/></div>
+        )
+    }
+
+}
+
+export default Keypad;
