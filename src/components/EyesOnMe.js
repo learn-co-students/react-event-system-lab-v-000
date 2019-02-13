c89145c79adff47adf3c1c23d19d3a7459a14e3c
Laura Correa <laura.correa@digitalunit.berlin>
2019-02-13 11:59:10 +0100

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React, {Component} from 'react';
+
+export default class EyesOnMe extends Component {
+    goodJobBuddy = () => {
+        console.log('Good!')
+    }
+    lookAtMe = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    render(){
+        return(
+            <div>
+                <button onFocus={this.goodJobBuddy} onBlur={this.lookAtMe}>
+                </button>
+            </div>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, {Component} from 'react';
+
+export default class Keypad extends Component {
+    print = () => {
+        console.log('Entering password...');
+    }
+    
+    render(){
+        return(
+            <div>
+                <label>password:</label>
+                <input type="password" onKeyUp={this.print}></input>
+            </div>
+        )
+    }
+}
