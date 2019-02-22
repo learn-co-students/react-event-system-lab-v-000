83b5236008ed7fbedeab3203cd8960ae08e01541
Madeline-Stark <madeline.stark1@gmail.com>
2019-02-21 17:08:41 -0800

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+  listenForFocus = () => (
+    console.log('Good!')
+  )
+
+  listenForBlur = () => (
+    console.log('Hey! Eyes on me!')
+  )
+
+
+
+  render() {
+    return (
+      <button
+      onFocus={this.listenForFocus}
+      onBlur={this.listenForBlur}
+      >
+      </button>
+    )
+  }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+  listenForKeyUp = () => (
+    console.log('Entering password...')
+  )
+
+
+
+  render() {
+    return (
+      <input type="password" onKeyUp={this.listenForKeyUp}/>
+    )
+  }
+
+}
