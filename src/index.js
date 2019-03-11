aa693c224acd3a8d9d596fd2439104582db72ebd
hejeong <jonhong777@gmail.com>
2019-03-11 02:22:12 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Keypad from './components/Keypad';
-import EyesOnMe from './components/EyesOnMe';
+import Keypad from './components/Keypad.js';
+import EyesOnMe from './components/EyesOnMe.js';
 
 ReactDOM.render(
   <div>
