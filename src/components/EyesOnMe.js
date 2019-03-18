adb886c89fefae350b9e5eae72e447ab51b1c876
Kkelly22 <kk504408@gmail.com>
2019-03-18 15:28:14 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+	watching = () => {
+		console.log("Good!")
+	}
+
+	drifting = () => {
+		console.log("Hey! Eyes on me!")
+	}
+  
+  render() {
+    return (
+    	<form>
+    		<button onFocus={this.watching} onBlur={this.drifting} />
+    	</form>
+    )
+  }
+  
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
-// Code Keypad Component Here
+
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+	enteringPassword = () => {
+		console.log("Entering password...")
+	}
+  
+	render() {
+		return (
+	    	<form>
+	    		<input type="password" onKeyUp={this.enteringPassword} />
+	    	</form>
+	    )
+	}
+  
+}
