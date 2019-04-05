8270f80d8c62d0b04051c8c93ea5ee7797ad8387
Jayon Kim <jpkim921@gmail.com>
2019-04-05 00:20:40 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+
+export default class EyesOnMe extends Component {
+
+  focusEventFn = () => {
+    console.log('Good!')
+  }
+  blurEventFn = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+
+
+  render () {
+    return (
+      <div>
+        <button onFocus={this.focusEventFn} onBlur={this.blurEventFn}>
+          Eyes On The Ball
+        </button>
+      </div>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+  keyUpEventFn = () => {
+    console.log('Entering password...')
+  }
+
+  render () {
+    return (
+      <div>
+        <input type="password" onKeyUp={this.keyUpEventFn}/>
+      </div>
+    )
+  }
+}
