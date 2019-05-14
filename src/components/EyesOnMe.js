e805065f9d629f8d3a344d14b552e0d3092d1b0a
Christopher P. Dundon <chris.dundon@gmail.com>
2019-05-13 22:51:57 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+  
+  constructor(props) {
+    super(props)
+  }
+
+  handleKey = () => (console.log("Entering password..."))
+	handleFocus = (e) => (console.log('Good!'))
+	handleBlur = (e) => (console.log('Hey! Eyes on me!'))	
+
+  render() {
+    return (
+      <div className="cell">
+				<button type="button" onFocus={e => this.handleFocus(e)} onBlur={e => this.handleBlur(e)}>Eyes On Me!</button>
+      </div>
+    )
+  }
+  
+}
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+  
+  constructor(props) {
+    super(props)
+  }
+
+	handleKey = () => (console.log("Entering password..."))
+
+  render() {
+    return (
+      <div className="cell">
+				<input type="password" onKeyUp={(e) => this.handleKey()} />
+      </div>
+    )
+  }
+  
+}
