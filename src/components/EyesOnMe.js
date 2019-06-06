888e06cffed3a84be38880c815cbfb5a5318b25b
Eclecticdynastye <bklyngal1994@gmail.com>
2019-06-06 19:47:06 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+  focusHandle = () => {
+    console.log("Good!")
+  }
+
+  blurHandle = () => {
+    console.log("Hey! Eyes on me!")
+  }
+
+  render() {
+    return (
+      <div>
+      <button
+      onFocus={this.focusHandle}
+      onBlur={this.blurHandle}>
+      Hey, eyes on me please!
+      </button>
+      </div>
+      )
+  }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+
+export deafult class Keypad extends Component {
+  enterPassword = () => {
+    console.log("Entering Password...")
+  }
+
+
+  render () {
+    return (
+      <div>
+      <input
+      type="password"
+      onKeyUp={this.enterPassword} />
+      </div>
+      )
+  }
+}
+
+
