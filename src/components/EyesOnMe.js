cd8f6a663682f3286321cbf2da667558e660f811
Christia Tidwell <christiatidwell@Christias-MBP.attlocal.net>
2019-06-12 16:41:09 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+  focusOn = () => {
+    console.log('Good!')
+  }
+
+  blurOn = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+    return (
+    <button
+    onFocus={this.focusOn}
+    onBlur={this.blurOn}
+    />
+   )
+  }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+  logKey = () => {
+    console.log('Entering password...')
+  }
+
+    render() {
+      return  (
+
+
+      <input
+       type="password"
+       onKeyUp={this.logKey}
+       />
+  )
+ }
+}
