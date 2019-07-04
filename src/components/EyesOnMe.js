f45662559c0f239c14a86a1c14d1d857010017f0
Rachel Wyatt <rw126@evansville.edu>
2019-07-04 11:04:13 -0400

    create Keypad component, handleKeyUp method

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+    render() {
+      return "TBD";
+    }
+
+}
+
+export default EyesOnMe
