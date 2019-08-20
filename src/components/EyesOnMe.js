b7388bf1226c6162fc74e36ecf216f22b1e6d831
inkanimal <theatomproject@gmail.com>
2019-08-20 14:55:50 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+  gHandler = () => {console.log('Good!')}
+
+  heyHandler = () => {console.log('Hey! Eyes on me!')}
+
+  render() {
+    return (
+      <button onFocus={this.gHandler} onBlur={this.heyHandler}>Button</button>
+    );
+  }
+}
