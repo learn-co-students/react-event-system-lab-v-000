06a1454b0fb708894119ed982f50d39d65574142
github username <github email address>
2019-08-29 22:25:12 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+// Code Keypad Component Here
+import React, { Component } from 'react';
+// import  from './';
+
+
+
+export default class EyesOnMe extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+
+
+  render() {
+    return (
+      <div className="">
+
+      </div>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+// import  from './';
+
+
+
+export default class Keypad extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+
+
+  render() {
+    return (
+      <div className="">
+        <input type="password" onKeyUp={console.log("Entering password")}/>
+      </div>
+    )
+  }
+}
