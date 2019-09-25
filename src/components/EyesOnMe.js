7a23de49311fd1813c9d3a442c19b540307d53de
John <veloblank@gmail.com>
2019-09-25 16:20:43 -0400

    Done. Just need to PR.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React, {Component} from 'react';
+
+class EyesOnMe extends Component{
+    handleOnFocus = ()=>{console.log("Good!")}
+    handleOnBlur = () =>{console.log("Hey! Eyes on me!")}
+    render(){
+
+        
+        return(
+            <button 
+            onFocus={this.handleOnFocus}
+            onBlur={this.handleOnBlur}
+            >Look at Me!</button>
+        )
+    }
+}
+
+export default EyesOnMe;
