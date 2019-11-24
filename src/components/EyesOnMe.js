e454148626339f89d689e0477ddd430ff3e58770
John Blank <veloblank@gmail.com>
2019-11-24 02:29:48 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+
+  onblur() {
+    console.log("Hey! Eyes on me!")
+  }
+
+  onfocus() {
+    console.log("Good!")
+  }
+  render() {
+    return (
+      <button onFocus={this.onfocus} onBlur={this.onblur}></button>
+    );
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React, { Component } from "react";
+
+class Keypad extends Component {
+  keyup() {
+    console.log("Entering password...");
+  }
+
+  render() {
+    return (
+      <div class="keypad-input">
+        <input type="password" onKeyUp={this.keyup} />
+      </div>
+    );
+  }
+}
+
+export default Keypad;
