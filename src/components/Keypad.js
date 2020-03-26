6b59dd2fca60955804f0fc151f71b5bb440f5b68
Rebecca Tong <rtong@buffalo.edu>
2020-03-26 14:55:21 -0400

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+
+import React, { Component } from 'react';
+
+
+
+export default class Keypad extends React.Component{
+  touchKey = () => {
+    console.log('Entering password...')
+  }
+  render(){
+    return(
+    <div>
+      <input onKeyUp={this.touchKey}
+      type="password">
+      Password</input>
+    </div>
+    )
+  }
+}
