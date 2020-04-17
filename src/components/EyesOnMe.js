30a2bb6906dbc287e7b2205289a1a88356e52dd4
leothebeagle <a.kosber@gmail.com>
2020-04-16 20:47:41 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+function focusLog() {
+    console.log('Good!')
+}
+
+function blurLog() {
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe(props) {
+    return <button onFocus={focusLog} onBlur={blurLog}></button>
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+function logMessage() {
+    console.log('Entering password...')
+}
+
+function Keypad(props) {
+    return <input onKeyUp={logMessage} type="password" />
+};
+
+export default Keypad;
