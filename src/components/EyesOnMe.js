76f8ff81ea7500cadc90a5a91260eabb3a5d71cd
mat_anderson <matandersonlv@hotmail.com>
2020-05-21 17:24:22 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+  isFocused = () => {
+    console.log('Good!')
+  }
+
+  isBlurred = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+    let message = this.isBlurred ? 'Hey! Eyes on me!' : 'Good!'
+    return (
+    <button onFocus={this.isFocused} onBlur={this.isBlurred}>{message}</button>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+  handleKeyPressed = () => {
+    console.log('Entering password...')
+  }
+
+  render() {
+    return (
+      <div >
+        <input type="password" onKeyUp={this.handleKeyPressed}/>
+      </div>
+    )
+  }
+  
+}
