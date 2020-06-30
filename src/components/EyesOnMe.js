f9a6cd55544755d9e82d24d89cdc2d49843d99b2
iamtamtamiam <vutam_a@hotmail.com>
2020-06-30 10:29:58 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+    eyesOn = () => {
+        console.log('Good!');
+      }
+
+      eyesOff = () => {
+        console.log('Hey! Eyes on me!');
+      }
+     
+
+
+    render() {
+        return (
+          <button onFocus={this.eyesOn} onBlur={this.eyesOff}> Eyes on me?</button>
+        );
+      }
+    
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+    keypadUp = () => {
+        console.log('Entering password...');
+      }
+     
+
+
+    render() {
+        return (
+          <form>
+            <input type="password" onKeyUp={this.keypadUp}></input>
+          </form>
+        );
+      }
+    
+}
