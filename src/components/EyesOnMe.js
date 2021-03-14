fe17071ff49fa3e25e7cc1c9da17986b17c61d1f
ricecake88 <gracelearnsforever@gmail.com>
2021-03-14 15:51:44 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component{
+
+    handleFocus = (event) => {
+        console.log('Good!');
+    }
+
+    handleBlur = (event) => {
+        console.log('Hey! Eyes on me!');
+    }
+
+    render() {
+        return <div><button onFocus={this.handleFocus} onBlur={this.handleBlur}/></div>
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component{
+
+    passwordPrint = (event) => {
+        console.log('Entering password...');
+    }
+
+    render() {
+        return <div><input type="password" onKeyUp={this.passwordPrint}/></div>
+    }
+}
