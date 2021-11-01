b41841b83ca52ac96e8becc399c0b88ed168142a
Elvin <Elvin@ElvinGarcia.com>
2021-11-01 12:55:10 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+class EyesOnMe extends React.Component{
+
+  render() {
+    return (
+      <button value="submit" onFocus={() => { console.log('Good!') }} onBlur={() => { console.log('Hey! Eyes on me!') }}  >
+        Submit
+      </button>
+    )
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+import React from "react";
+
+class Keypad extends React.Component{
+  constructor() {
+    super();
+  }
+
+  render() {
+    return ( <input type="password" onKeyUp={ ()=>{console.log('Entering password...')}} /> )
+  }
+
+}
+
+export default Keypad;
